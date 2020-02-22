a910ebcea377a112b638bb8e2c0aa079517ebb45
Nate-Mantell <mantelln@gmail.com>
2020-02-22 18:23:45 -0500

    Add portfolio page

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/portfolio/">Go to page 2</Link>
+    <Link to="/portfolio/">Portfolio</Link>
   </Layout>
 )
 
