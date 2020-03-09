a72dbd6774bf054bd26cec2413bf4467c28a0e04
Nate-Mantell <mantelln@gmail.com>
2020-03-09 20:43:45 +0700

    Add style module to blog post template to change styling of post info

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,6 +3,8 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 
+import styles from "./blog-post.module.scss"
+
 export default function Template({ data }) {
   const { markdownRemark } = data
   const { frontmatter, html } = markdownRemark
@@ -10,11 +12,21 @@ export default function Template({ data }) {
   return (
     <Layout>
       <div>
-        <h1>{frontmatter.title}</h1>
-        <h4>
-          Posted by {frontmatter.author} on {frontmatter.date}
+        <h1 className={styles["post__title"]}>{frontmatter.title}</h1>
+        <h4 className={styles["post__info"]}>
+          Posted by{" "}
+          <span className={styles["post__info__important"]}>
+            {frontmatter.author}
+          </span>{" "}
+          on{" "}
+          <span className={styles["post__info__important"]}>
+            {frontmatter.date}
+          </span>
         </h4>
-        <div dangerouslySetInnerHTML={{ __html: html }}></div>
+        <div
+          className={styles["post__body"]}
+          dangerouslySetInnerHTML={{ __html: html }}
+        ></div>
       </div>
     </Layout>
   )
