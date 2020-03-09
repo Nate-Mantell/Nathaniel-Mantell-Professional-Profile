f671f55eecdffde06b5c3fedcd69af1d8884fac9
Nate-Mantell <mantelln@gmail.com>
2020-03-09 22:01:35 +0700

    Add horizontal margin on blog post list

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,28 +4,34 @@ import { Link } from "gatsby"
 
 import Layout from "../components/Layout"
 
+import styles from "./blog.module.scss"
+
 const Blog = ({ data }) => (
   <Layout
     title="Blog"
     description="Articles about web software development"
     socialImage="photo.jpg"
   >
-    <h1>Latest Posts</h1>
-    {data.allMarkdownRemark.edges.map(post => (
-      <div key={post.node.id}>
-        <h3>{post.node.frontmatter.title}</h3>
-        <small>
-          Posted by {post.node.frontmatter.author} on{" "}
-          {post.node.frontmatter.date}
-        </small>
-        <br />
-        <br />
-        <Link to={post.node.frontmatter.path}>Read More</Link>
-        <br />
-        <br />
-        <hr />
+    <div className={styles["container"]}>
+      <h1>Latest Posts</h1>
+      <div className={styles["post_list"]} id="blogPostList">
+        {data.allMarkdownRemark.edges.map(post => (
+          <div key={post.node.id}>
+            <h3>{post.node.frontmatter.title}</h3>
+            <small>
+              Posted by {post.node.frontmatter.author} on{" "}
+              {post.node.frontmatter.date}
+            </small>
+            <br />
+            <br />
+            <Link to={post.node.frontmatter.path}>Read More</Link>
+            <br />
+            <br />
+            <hr />
+          </div>
+        ))}
       </div>
-    ))}
+    </div>
   </Layout>
 )
 
