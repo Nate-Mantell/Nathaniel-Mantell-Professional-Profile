de7ff187026e7f5ed85d7168151d1a331dda00e3
Nate-Mantell <mantelln@gmail.com>
2020-03-11 21:39:50 +0700

    Add ajax request to contact form to display success or failure message on page after submission

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,72 +5,115 @@ import Layout from "../components/Layout"
 import styles from "./contact.module.scss"
 import "./contact.css"
 
-const ContactPage = () => (
-  <Layout
-    title="Contact"
-    description="Nathaniel Mantell is a web software developer - contact me!"
-    socialImage="photo.jpg"
-  >
-    <h1>Contact Me</h1>
-    <p>
-      You can contact me about anything! Need a website built? An application? A
-      pizza delivered?
-    </p>
-    <p>Use the contact form below or send me an email at mantelln@gmail.com</p>
+class ContactPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.submitForm = this.submitForm.bind(this)
+    this.state = {
+      status: "",
+    }
+  }
 
-    <div class="form-style-2">
-      <form method="post" action="https://formspree.io/mpzlarkl">
-        <label for="name">
-          <span>
-            Name <span class="required">*</span>
-          </span>
-          <input type="text" class="input-field" name="name" />
-        </label>
-        <label for="_replyto">
-          <span>
-            Email <span class="required">*</span>
-          </span>
-          <input type="text" class="input-field" name="_replyto" />
-        </label>
-        <label>
-          <span>Telephone</span>
-          <input
-            type="text"
-            class="tel-number-field"
-            name="tel_no_1"
-            maxlength="4"
-          />
-          -
-          <input
-            type="text"
-            class="tel-number-field"
-            name="tel_no_2"
-            maxlength="4"
-          />
-          -
-          <input
-            type="text"
-            class="tel-number-field"
-            name="tel_no_3"
-            maxlength="10"
-          />
-        </label>
-        <label for="message">
-          <span>
-            Message <span class="required">*</span>
-          </span>
-          <textarea name="message" class="textarea-field"></textarea>
-        </label>
-        <input type="text" name="_gotcha" className={styles["hidden"]} />
+  render() {
+    const { status } = this.state
+    return (
+      <Layout
+        title="Contact"
+        description="Nathaniel Mantell is a web software developer - contact me!"
+        socialImage="photo.jpg"
+      >
+        <h1>Contact Me</h1>
+        <p>
+          You can contact me about anything! Need a website built? An
+          application? A pizza delivered?
+        </p>
+        <p>
+          Use the contact form below or send me an email at mantelln@gmail.com
+        </p>
 
-        <label>
-          <span> </span>
-          <input type="submit" value="Submit" />
-        </label>
-      </form>
-    </div>
-  </Layout>
-)
+        <div class="form-style-2">
+          <form
+            onSubmit={this.submitForm}
+            action="https://formspree.io/mpzlarkl"
+            method="POST"
+          >
+            <label for="name">
+              <span>
+                Name <span class="required">*</span>
+              </span>
+              <input type="text" class="input-field" name="name" />
+            </label>
+            <label for="_replyto">
+              <span>
+                Email <span class="required">*</span>
+              </span>
+              <input type="text" class="input-field" name="_replyto" />
+            </label>
+            <label>
+              <span>Telephone</span>
+              <input
+                type="text"
+                class="tel-number-field"
+                name="tel_no_1"
+                maxlength="4"
+              />
+              -
+              <input
+                type="text"
+                class="tel-number-field"
+                name="tel_no_2"
+                maxlength="4"
+              />
+              -
+              <input
+                type="text"
+                class="tel-number-field"
+                name="tel_no_3"
+                maxlength="10"
+              />
+            </label>
+            <label for="message">
+              <span>
+                Message <span class="required">*</span>
+              </span>
+              <textarea name="message" class="textarea-field"></textarea>
+            </label>
+            <input type="text" name="_gotcha" className={styles["hidden"]} />
+
+            {status === "SUCCESS" ? (
+              <p>Thanks!</p>
+            ) : (
+              <label>
+                <span> </span>
+                <input type="submit" value="Submit" />
+              </label>
+            )}
+            {status === "ERROR" && <p>Ooops! There was an error.</p>}
+          </form>
+        </div>
+      </Layout>
+    )
+  }
+
+  submitForm(ev) {
+    ev.preventDefault()
+    const form = ev.target
+    const data = new FormData(form)
+    const xhr = new XMLHttpRequest()
+    xhr.open(form.method, form.action)
+    xhr.setRequestHeader("Accept", "application/json")
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return
+      if (xhr.status === 200) {
+        form.reset()
+        this.setState({ status: "SUCCESS" })
+      } else {
+        this.setState({ status: "ERROR" })
+      }
+    }
+    xhr.send(data)
+  }
+}
 
 export default ContactPage
 
