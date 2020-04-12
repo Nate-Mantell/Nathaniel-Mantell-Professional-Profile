9b7452ed9caa32495f234a3dcec7ac87ba59a58e
Nate-Mantell <mantelln@gmail.com>
2020-04-12 15:36:22 +0700

    Add Vesta graphics, section for Smiley360 blog, section for Medical Site Example to portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -72,12 +72,82 @@ const Portfolio = () => (
           Visit Social Media Link Website
         </a>
       </h4>
+      <div className={styles["gallery"]}>
+        <img
+          className={styles["gallery__image"]}
+          width="80%"
+          src={withPrefix(`Vesta-Platform-1.png`)}
+          alt="SML Vesta Platform example 1"
+        />
+        <img
+          className={styles["gallery__image"]}
+          width="80%"
+          src={withPrefix(`Vesta-Platform-2.png`)}
+          alt="SML Vesta Platform example 2"
+        />
+      </div>
+      <h2>Smiley360 Blog</h2>
+      <h4>
+        Wordpress Site -{" "}
+        <a
+          href="https://blog.smiley360.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Visit Smiley360 Blog
+        </a>
+      </h4>
+      <div className={styles["gallery"]}>
+        <img
+          className={styles["gallery__image"]}
+          width="80%"
+          src={withPrefix(`Smiley360-Wordpress-Blog-1.jpg`)}
+          alt="Smiley360 Wordpress example 1"
+        />
+        <img
+          className={styles["gallery__image"]}
+          width="80%"
+          src={withPrefix(`Smiley360-Wordpress-Blog-2.jpg`)}
+          alt="Smiley360 Wordpress example 2"
+        />
+        <img
+          className={styles["gallery__image"]}
+          width="80%"
+          src={withPrefix(`Smiley360-Wordpress-Blog-3.jpg`)}
+          alt="Smiley360 Wordpress Blog example 3"
+        />
+      </div>
+      <h2>Medical Site Example</h2>
+      <h4>
+        Wordpress Site -{" "}
+        <a
+          href="http://medical.mantellsolutions.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Visit the site
+        </a>
+      </h4>
+      <div className={styles["gallery"]}>
+        <img
+          className={styles["gallery__image"]}
+          width="80%"
+          src={withPrefix(`Medical-Site-Wordpress-1.jpg`)}
+          alt="Medical Site Wordpress example 1"
+        />
+        <img
+          className={styles["gallery__image"]}
+          width="80%"
+          src={withPrefix(`Medical-Site-Wordpress-2.jpg`)}
+          alt="Medical Site Wordpress example 2"
+        />
+      </div>
 
       <br />
 
       <h2>NathanielMantell.com</h2>
       <h4>Website</h4>
-      <p>This website was developed from scratch using GatsbyJS</p>
+      <p>This website was developed using GatsbyJS</p>
 
       <br />
       <Link to="/">Go back to the homepage</Link>
